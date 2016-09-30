6314bdfed479a1c75e750bb471fa832d80b2ed3e
Matt Parrett <matt@plainvanillagames.com>
2016-09-30 13:39:48 -0700

    Cleanup, fix tests

--- src/MP/Framework/CouponCode.php
+++ src/MP/Framework/CouponCode.php
@@ -8,28 +8,34 @@ namespace MP\Framework;
  */
 class CouponCode
 {
-    private static $BITCOUNT = 30;
-    public static $BITMASK   = 32767; // (1 << (30 >> 1)) - 1
-           public static $ALPHABET  = "XUF57ESCYK620D8QZNL3RHMG4W9PJTVA";
-    public static $MAX       = 4294967296; // 2 << 31;
+    private static $BITCOUNT = 30; // Number of bits in code
+    public static $BITMASK   = 32767; // (1 << ($BITCOUNT >> 1)) - 1
+    public static $ALPHABET  = "XUF57ESCYK620D8QZNL3RHMG4W9PJTVA"; // Random shuffled alphabet
+    public static $MAX       = 4294967296; // 2 << ($BITCOUNT + 1);
 
-           public static function init()
-           {
-               //self::$BITMASK = (1 << self::$BITCOUNT/2) - 1;
-           }
+    /**
+     * TODO
+     */
+    public static function init()
+    {
+        //self::$BITMASK = (1 << self::$BITCOUNT/2) - 1;
+    }
 
     public static function hashFunction($num)
     {
         return ((($num ^ 47894) + 25) << 1) & self::$BITMASK;
     }
 
+    /**
+     * Hash function to scramble the bits of $num before converting to a coupon code
+     */
     public static function hash($num)
     {
         $left = $num >> (self::$BITCOUNT/2);
         $right = $num & self::$BITMASK;
 
         for ($round = 0; $round < 9; ++$round) {
-            $left = $left ^ self::roundFunction($right); // Left XOR with round func
+            $left = $left ^ self::hashFunction($right); // Left XOR with round func
                    $temp = $left;
             $left = $right;
             $right = $temp; // Swap
@@ -37,6 +43,10 @@ class CouponCode
         return $left | ($right << (self::$BITCOUNT/2));
     }
 
+    /**
+     * Gets a string code from a coupon ID
+     * @arg $number integer coupon ID
+     */ 
     public static function getCode($number)
     {
         $b = "";
@@ -47,6 +57,10 @@ class CouponCode
         return $b;
     }
 
+    /**
+     * Generate an integer code from a string coupon
+     * @arg $coupon string 
+     */
     public static function codeFromCoupon($coupon)
     {
         str_replace('O', '0', $coupon);
@@ -57,6 +71,9 @@ class CouponCode
         return $n;
     }
 
+    /**
+     * Sshuffle items using a seed 
+     */
     public static function seeded_shuffle(&$items, $seed = false)
     {
         $items = array_values($items);
@@ -67,6 +84,9 @@ class CouponCode
         }
     }
 
+    /**
+     * Unshuffle items using a seed 
+     */
     public static function seeded_unshuffle(&$items, $seed)
     {
         $items = array_values($items);
@@ -82,6 +102,10 @@ class CouponCode
         }
     }
 
+    /**
+     * Shuffle the alphabet using a seed. This is useful if you want previous codes
+     * to change at regular intervals. Just rotate your seed.
+     */
     public static function shuffleAlphabet($seed)
     {
         $chars = str_split(self::$ALPHABET);
@@ -90,18 +114,26 @@ class CouponCode
     }
 }
 
+// CLI tests / sample usage
 if (isset($argv) && $argc > 1) {
     CouponCode::init();
     CouponCode::shuffleAlphabet(1);
     CouponCode::$MAX = 10;
+    printf("%10s\t%10s\t%s\t%s\n", "Input num", "Hashed", "Code", "Recovered num");
+    printf("------------------------------------------------------\n");
+
+    //for($i=0;$i<PHP_INT_MAX;$i++) { /* full test */
     for ($i=CouponCode::$MAX;$i>=0;$i--) {
-        //for($i=0;$i<PHP_INT_MAX;$i++) {
-               $c = CouponCode::hash($i);
-        echo sprintf("%d\t%d\t%d\t%s\t%s\n",
-            $i, $c,
+        $c = CouponCode::hash($i);
+        $recovered = CouponCode::hash(CouponCode::codeFromCoupon(CouponCode::getCode($c)));
+        assert ( $recovered == $i);
+        
+        printf("%10d\t%10d\t%d\t%s\t%s\n",
+            $i, 
+            $c,
             CouponCode::hash($c),
             CouponCode::getCode($c),
-            CouponCode::hash(CouponCode::codeFromCoupon(CouponCode::getCode($c)))
-            );
+            $recovered
+        );
     }
 }
