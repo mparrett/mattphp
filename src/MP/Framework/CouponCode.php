a2b9877f710bea7f5769b64b15d1fbc206fb0660
Matt Parrett <matt@plainvanillagames.com>
2016-09-30 13:31:24 -0700

    Add CouponCode and Stegano classes

---
+++ src/MP/Framework/CouponCode.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace MP\Framework;
+
+/**
+ * Coupon code generator
+ * Inspired by http://stackoverflow.com/questions/11708559/coupon-code-generation
+ */
+class CouponCode
+{
+    private static $BITCOUNT = 30;
+    public static $BITMASK   = 32767; // (1 << (30 >> 1)) - 1
+           public static $ALPHABET  = "XUF57ESCYK620D8QZNL3RHMG4W9PJTVA";
+    public static $MAX       = 4294967296; // 2 << 31;
+
+           public static function init()
+           {
+               //self::$BITMASK = (1 << self::$BITCOUNT/2) - 1;
+           }
+
+    public static function hashFunction($num)
+    {
+        return ((($num ^ 47894) + 25) << 1) & self::$BITMASK;
+    }
+
+    public static function hash($num)
+    {
+        $left = $num >> (self::$BITCOUNT/2);
+        $right = $num & self::$BITMASK;
+
+        for ($round = 0; $round < 9; ++$round) {
+            $left = $left ^ self::roundFunction($right); // Left XOR with round func
+                   $temp = $left;
+            $left = $right;
+            $right = $temp; // Swap
+        }
+        return $left | ($right << (self::$BITCOUNT/2));
+    }
+
+    public static function getCode($number)
+    {
+        $b = "";
+        for ($i=0; $i<6; ++$i) {
+            $b .= self::$ALPHABET[$number & ((1 << 5)-1)];
+            $number = $number >> 5;
+        }
+        return $b;
+    }
+
+    public static function codeFromCoupon($coupon)
+    {
+        str_replace('O', '0', $coupon);
+        $n = 0;
+        for ($i = 0; $i < 6; ++$i) {
+            $n = $n | strpos(self::$ALPHABET, $coupon[$i]) << (5 * $i);
+        }
+        return $n;
+    }
+
+    public static function seeded_shuffle(&$items, $seed = false)
+    {
+        $items = array_values($items);
+        mt_srand($seed ? $seed : time());
+        for ($i = count($items) - 1; $i > 0; $i--) {
+            $j = mt_rand(0, $i);
+            list($items[$i], $items[$j]) = array($items[$j], $items[$i]);
+        }
+    }
+
+    public static function seeded_unshuffle(&$items, $seed)
+    {
+        $items = array_values($items);
+
+        mt_srand($seed);
+        $indices = array();
+        for ($i = count($items) - 1; $i > 0; $i--) {
+            $indices[$i] = mt_rand(0, $i);
+        }
+
+        foreach (array_reverse($indices, true) as $i => $j) {
+            list($items[$i], $items[$j]) = array($items[$j], $items[$i]);
+        }
+    }
+
+    public static function shuffleAlphabet($seed)
+    {
+        $chars = str_split(self::$ALPHABET);
+        self::seeded_shuffle($chars, $seed ^ 59721);
+        self::$ALPHABET = join('', $chars);
+    }
+}
+
+if (isset($argv) && $argc > 1) {
+    CouponCode::init();
+    CouponCode::shuffleAlphabet(1);
+    CouponCode::$MAX = 10;
+    for ($i=CouponCode::$MAX;$i>=0;$i--) {
+        //for($i=0;$i<PHP_INT_MAX;$i++) {
+               $c = CouponCode::hash($i);
+        echo sprintf("%d\t%d\t%d\t%s\t%s\n",
+            $i, $c,
+            CouponCode::hash($c),
+            CouponCode::getCode($c),
+            CouponCode::hash(CouponCode::codeFromCoupon(CouponCode::getCode($c)))
+            );
+    }
+}
---
+++ src/MP/Framework/Stegano.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace MP\Framework;
+
+/**
+ * MattPHP
+ * Fun steganographic utilities
+ * @author Matt Parrett
+ */
+class Stegano
+{
+
+    /**
+     * Simple 16 bit hash function (132 + k + (32 * id))
+     */
+    public static function simpleHash($id, $k)
+    {
+        return (132 + $k + (($id << 5) % 65536) + $id) % 65536;
+    }
+
+    /**
+      * Generate a scrambled version of the input ID using a Feistel Cipher
+      *
+      * MySQL max for pid is 4294967295 (32 bits)
+      */
+    public static function scrambleID($id, $rnd, $reverse = false)
+    {
+        $lo16mask = 0xffff;
+        $hi16mask = 0xffff0000;
+
+        $lo = $id & $lo16mask;
+        $hi = $id & $hi16mask;
+
+        //echo '<pre>'; var_dump(dechex($id)); var_dump(dechex($hi)); var_dump(dechex($lo));
+
+        if ($reverse) {
+            $hihash = self::simpleHash($hi >> 16, $rnd);
+            $lo ^= $hihash;
+            $ret = (($hi >> 16) & $lo16mask) | (($lo << 16) & $hi16mask);
+        } else {
+            $lohash = self::simpleHash($lo, $rnd);
+            $hi ^= $lohash << 16;
+            $ret = (($hi >> 16) & $lo16mask) | (($lo << 16) & $hi16mask);
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Scramble an ID using a Feistel cipher
+     * @arg  $kvals  array of values to use for the rounds of ciphering
+     */
+    public static function scramble($id, array $kvals)
+    {
+        $out = $id;
+        for ($i = 0, $len = count($kvals); $i < $len; $i++) {
+            $out = Stegano::scramblePid($out, $kvals[$i]);
+        }
+        return $out;
+    }
+
+    /**
+     * Unscramble an ID using a Feistel cipher
+     * @arg  $kvals  array of values to use for the rounds of deciphering
+     * Must match values used to encrypt.
+     */
+    public static function unscramble($id, array $kvals)
+    {
+        $out = $id;
+        for ($i = count($kvals) - 1; $i >= 0; $i--) {
+            $out = Stegano::scramblePid($out, $kvals[$i], true);
+        }
+        return $out;
+    }
+
+    /**
+     * Hides an integer in a MD5-like string
+     * @arg  $id  32 bit integer
+     */
+    public static function disguiseLikeMD5($id)
+    {
+        $binpid = str_pad(decbin($id), 32, "0", STR_PAD_LEFT);
+
+        $str = '';
+        $hexchars_on = '13579ace';
+        $hexchars_off = '24680bdf';
+
+        for ($i = 0; $i < 32; $i++) {
+            if ($binpid[$i]) { // set bit
+                $str .= $hexchars_on[ mt_rand(0, 7)];
+            } else { // unset bit
+                $str .= $hexchars_off[ mt_rand(0, 7)];
+            }
+        }
+        return $str;
+    }
+
+   /**
+    * Recovers a 32 bit integer from a faux MD5 hash
+    * @arg  $md5  md5 hash string
+    */
+    public static function recoverFromMD5($md5)
+    {
+        for ($i = 0; $i < 32; $i++) {
+            $md5[$i] = ord($md5[$i]) & 0x01;
+        }
+        return bindec($md5);
+    }
+}
